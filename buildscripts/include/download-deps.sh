dc538a1d8185558a0fd2f4ec29434f201725fba2
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-02-10 02:12:31 -0400

    Update download-deps.sh

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -61,6 +61,6 @@ fi
 [ ! -d libplacebo ] && git clone --recursive https://github.com/haasn/libplacebo
 
 # mpv
-[ ! -d mpv ] && git clone https://github.com/mpv-player/mpv
+[ ! -d mpv ] && git clone https://github.com/Clouddark75/mpv
 
 cd ..
