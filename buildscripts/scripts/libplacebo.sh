88aaae67e9cdf680dd0cf31b0e321ad5aa3bc6f0
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-03-22 02:12:22 -0400

    Update libplacebo.sh

--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -13,6 +13,16 @@ else
 	exit 255
 fi
 
+# Android provides Vulkan, but no pkgconfig file
+mkdir -p "$prefix_dir"/lib/pkgconfig
+cat >"$prefix_dir"/lib/pkgconfig/vulkan.pc <<"END"
+Name: Vulkan
+Description:
+Version: 1.3.275
+Libs: -lvulkan
+Cflags:
+END
+
 unset CC CXX
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
 	-Dvk-proc-addr=enabled -Ddemos=false
